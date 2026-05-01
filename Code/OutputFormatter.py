e8fc6f46244e212d883383182dea61eb39e20d2f
Michaela Licen <26417499@sun.ac.za>
2026-05-01 11:53:25 +0200

    cleaning repo

--- Code/OutputFormatter.py
+++
@@ -1,7 +0,0 @@
-'''
-    Take the final solution from cp-sat and format it for output in excel
-'''
-
-def write_roster(solution, excel_file):
-    # Format the solution for output in Excel
-    pass
